df003d81e3a3ba7f2267acaf359ddc27a63c139e
Tim Gage <tsg21@users.noreply.github.com>
2020-11-22 21:44:36 +0000

    Can get access tokens!

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { msalInstance } from './msalInstance';
 import Content from './Content';
 import { useEffect, useState } from 'react';
+import { InteractionRequiredAuthError, LogLevel } from '@azure/msal-browser';
 
 const App = () =>  {
   const [tokenResponse, setTokenResponse] = useState(null)
@@ -24,15 +25,39 @@ const App = () =>  {
     })
   }, [])
 
+  useEffect(() => {
+    if (loginState !== "loggedin") {
+      return
+    }
+
+    var request = {
+        scopes: ["api://backend1/Read", "api://backend1/Write"],
+        account: msalInstance.getAllAccounts()[0],
+    };
+    
+    msalInstance.acquireTokenSilent(request).then(tokenResponse => {
+        console.log("Access token: ", tokenResponse)
+    }).catch(error => {
+        if (error instanceof InteractionRequiredAuthError) {
+            // fallback to interaction when silent call fails
+            return msalInstance.acquireTokenRedirect(request)
+        } else {
+          console.log(error)
+        }
+    });
+  }, [loginState])
+
   const dologin = () => {
       try {
-        msalInstance.loginRedirect({});
+        var loginRequest = {
+          scopes: ["user.read", "api://backend1/Read", "api://backend1/Write"]
+        };
+
+        msalInstance.loginRedirect(loginRequest);
       } catch (err) {
           console.log(err)
       }
-  }
-  console.log("setup") 
-  
+  }  
   return <><p>{loginState}</p>
     <button onClick={dologin}>login</button>
   </>
--- src/msalInstance.js
+++ src/msalInstance.js
@@ -2,7 +2,12 @@ import * as msal from "@azure/msal-browser";
 
 const msalConfig = {
     auth: {
-        clientId: '7760fcac-c585-42bd-9e52-616cec796589'
+        clientId: '7760fcac-c585-42bd-9e52-616cec796589',
+        authority: 'https://login.microsoftonline.com/c145a4fe-8f5a-44fe-b8ac-7aa36c863a87'
+    },
+    cache: {
+      cacheLocation: 'localStorage',
+      storeAuthStateInCookie: true
     }
 };
 
